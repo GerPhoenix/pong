be01a94963956668b25a6e4cf980688b986bd699
Jonas <jonashoefler@gmx.de>
2020-03-27 14:10:15 +0100

    training configuration

--- agent/train.py
+++ agent/train.py
@@ -24,11 +24,11 @@ obs_dim = env.observation_space.shape[0]
 # deep network
 model = Sequential()
 model.add(Flatten(input_shape=(1,) + env.observation_space.shape))
-model.add(Dense(64))
+model.add(Dense(128))
 model.add(Activation('relu'))
-model.add(Dense(32))
+model.add(Dense(128))
 model.add(Activation('relu'))
-model.add(Dense(32))
+model.add(Dense(128))
 model.add(Activation('relu'))
 model.add(Dense(nb_actions))
 model.add(Activation('linear'))
@@ -40,18 +40,37 @@ except:
 # Finally, we configure and compile our agent. You can use every built-in tensorflow.keras optimizer and
 # even the metrics!
 memory = SequentialMemory(limit=100000, window_length=1)
-policy = DecayingEpsGreedyQPolicy(eps=0.6, decay=0.99995)
+policy = DecayingEpsGreedyQPolicy(eps=0.35)
 dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=1000,
-               target_model_update=0.005, policy=policy)
-dqn.compile(Adam(lr=1e-3), metrics=['mae'])
+               target_model_update=0.05, policy=policy)
+dqn.compile(Adam(lr=1e-2), metrics=['mae'])
 
+cp_callback = ModelCheckpoint(filepath="data/dqn_pong_params.h5f",
+                              save_weights_only=True,
+                              verbose=1)
+dqn.fit(env, nb_steps=15000, verbose=2, visualize=False, callbacks=[cp_callback])
+
+policy = DecayingEpsGreedyQPolicy(eps=0.2)
+dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=1000,
+               target_model_update=0.035, policy=policy)
+dqn.compile(Adam(lr=1e-2), metrics=['mae'])
+dqn.fit(env, nb_steps=30000, verbose=2, visualize=False, callbacks=[cp_callback])
+
+policy = DecayingEpsGreedyQPolicy(eps=0.1)
+dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=1000,
+               target_model_update=0.02, policy=policy)
+dqn.compile(Adam(lr=1e-3), metrics=['mae'])
 # Okay, now it's time to learn something using the fit function! You can visualize the training here, but this
 # slows down training quite a lot.
 # You can abort the training without much progress being lost if the cp_callback is bound
 #
-cp_callback = ModelCheckpoint(filepath="data/dqn_pong_params.h5f",
-                              save_weights_only=True,
-                              verbose=1)
-dqn.fit(env, nb_steps=100001, verbose=2, visualize=False, callbacks=[cp_callback])
+dqn.fit(env, nb_steps=30000, verbose=2, visualize=False, callbacks=[cp_callback])
+
+policy = DecayingEpsGreedyQPolicy(eps=0.05)
+dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=1000,
+               target_model_update=1e-2, policy=policy)
+dqn.compile(Adam(lr=1e-3), metrics=['mae'])
+dqn.fit(env, nb_steps=25000, verbose=2, visualize=False, callbacks=[cp_callback])
+
 # After training is done, we save the best weights.
 dqn.save_weights("data/dqn_pong_params.h5f", overwrite=True)
--- gym-repo/gym_repo/envs/PongEnv.py
+++ gym-repo/gym_repo/envs/PongEnv.py
@@ -12,9 +12,9 @@ BLUE = (0, 0, 255)
 SIZE = (800, 600)
 PLATFORM_X_DEFAULT = 350
 PLATFORM_Y_DEFAULT = 580
-PLATFORM_VELOCITY = 6
-BALL_VELOCITY_X = 6
-BALL_VELOCITY_Y = 5
+PLATFORM_VELOCITY = 12
+BALL_VELOCITY_X = 10
+BALL_VELOCITY_Y = 8
 
 BALL_SIZE = 15
 PLATFORM_SIZE_X = 100
@@ -26,6 +26,7 @@ class PongEnv(gym.Env):
 
     def __init__(self):
         # ENV INIT
+        self.previous_action = 0
         self.action_space = spaces.Discrete(3)
         self.observation_space = spaces.Box(0, 800, shape=(5,), dtype=np.float32)
 
@@ -64,8 +65,9 @@ class PongEnv(gym.Env):
                 self.platform_change_x = PLATFORM_VELOCITY
             self.move_platform()
         self.move_ball()
-        reward = self.calculate_reward(old_score)
+        reward = self.calculate_reward(old_score, action)
         # if ball was catched 10 times end episode
+        self.previous_action = action
         if self.score == 10:
             self.episode_over = True
         return self.pack_observation(), reward, self.episode_over, {}
@@ -108,7 +110,7 @@ class PongEnv(gym.Env):
         self.screen.blit(text, [600, 100])
 
         pygame.display.flip()
-        self.clock.tick(180)
+        self.clock.tick(90)
 
     def close(self):
         pygame.quit()
@@ -117,9 +119,9 @@ class PongEnv(gym.Env):
     def move_platform(self):
         self.platform_x += self.platform_change_x
         self.platform_y += self.platform_change_y
-        if self.platform_x <= 0:
+        if self.platform_x < 0:
             self.platform_x = 0
-        if self.platform_x >= 699:
+        if self.platform_x > 699:
             self.platform_x = 699
 
     def draw_platform(self):
@@ -140,9 +142,10 @@ class PongEnv(gym.Env):
         if self.ball_y < 0:
             self.ball_y = 0
             self.ball_change_y = self.ball_change_y * -1
-        elif self.platform_x < self.get_ball_x_position() < self.platform_x + PLATFORM_SIZE_X \
+        elif -(BALL_SIZE / 2) + self.platform_x < self.get_ball_x_position() < \
+                self.platform_x + PLATFORM_SIZE_X + BALL_SIZE / 2 \
                 and PLATFORM_Y_DEFAULT - PLATFORM_SIZE_Y < self.ball_y < PLATFORM_Y_DEFAULT - 10:
-            self.ball_y = 560
+            self.ball_y = 564
             self.ball_change_y = self.ball_change_y * -1
             self.score += 1
         elif self.ball_y > 600:
@@ -156,14 +159,16 @@ class PongEnv(gym.Env):
         # Add half of the platform length because the render will be to the right
         return self.platform_x + 50
 
-    def calculate_reward(self, old_score):
+    def calculate_reward(self, old_score, action):
         reward = 0
         # penalty for being not close to the ball
-        # reward = -abs(self.get_ball_x_position() - self.get_platform_x_position())
+        reward = -min((abs(self.get_ball_x_position() - self.get_platform_x_position()) - 5), 40)
+        if action != 0 or self.previous_action != action:
+            reward -= 50
         # if ball was missed add penalty
         if self.episode_over:
-            reward = -200000
+            reward = -20000
         # if ball was caught add reward
         if old_score < self.score:
-            reward = 500000
+            reward = 100000
         return reward
